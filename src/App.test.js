8918b6e6bae075578d8ba7e6bef8915d33fcbac5
Huiling97 <71744836+Huiling97@users.noreply.github.com>
2020-10-06 20:40:15 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,7 @@ import { render } from '@testing-library/react';
 import App from './App'; // imports app file
 
 test('renders learn react link', () => {
-  const { getNodeText } = render(<App />);
-
-    function getNodeText(node) {
+  const { getNodeText(node) } = render(<App />) {
       if (node.matches('input[type=submit], input[type=button]')) {
         return node.value
       }
