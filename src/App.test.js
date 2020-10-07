04de111c9bfa670d4cb1d62f807d5e7bfe865914
Huiling97 <71744836+Huiling97@users.noreply.github.com>
2020-10-07 13:11:06 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App'; // imports app file
 
 test('renders learn react link', () => {
   
-    screen.getByText((content, node) => {
+      getByText((content, node) => {
       const hasText = (node) => node.textContent === "Welcome to FintechSG React Course";
       const nodeHasText = hasText(node);
       const childrenDontHaveText = Array.from(node.children).every(
